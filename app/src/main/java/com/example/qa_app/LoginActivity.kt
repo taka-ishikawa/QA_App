d08d403cacb05b73a05246e1989ae6754df5383d
Takahiro Ishikawa <t-ishikawa@i-enter.co.jp>
2019-04-26 17:46:11 +0900

    [update] LoginActivity.kt
    set instances/ outlines of methods in this activity.

--- app/src/main/java/com/example/qa_app/LoginActivity.kt
+++ app/src/main/java/com/example/qa_app/LoginActivity.kt
@@ -2,11 +2,64 @@ package com.example.qa_app
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.view.View
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mCreateAccountListener:OnCompleteListener<AuthResult>
+    private lateinit var mLoginListener: OnCompleteListener<AuthResult>
+    private lateinit var mDataBaseReference: DatabaseReference
+
+    // アカウント作成時にフラグを立て、ログイン処理後に名前をFirebaseに保存する
+    private var mIsCreateAccount = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        mDataBaseReference = FirebaseDatabase.getInstance().reference
+
+        // FirebaseAuthのオブジェクトを取得する
+        mAuth = FirebaseAuth.getInstance()
+
+        mCreateAccountListener = OnCompleteListener { task ->
+            if (task.isSuccessful) { // login
+                val email = editTextEmail.text.toString()
+                val password = editTextPass.text.toString()
+                login(email, password)
+            } else { //display "error"
+                val view = findViewById<View>(android.R.id.content)
+                Snackbar.make(view, "アカウント作成に失敗しました", Snackbar.LENGTH_LONG).show()
+                progressBar.visibility = View.GONE
+            }
+        }
+
+        mLoginListener = OnCompleteListener { task ->
+            if (task.isSuccessful) {
+
+            } else {
+
+            }
+        }
+
+        title = "ログイン"
+
+        buttonLogin.setOnClickListener {
+
+        }
+        buttonResister.setOnClickListener { }
     }
+
+    fun createAccount() { }
+
+    fun login(email: String, password: String) { }
+
 }
