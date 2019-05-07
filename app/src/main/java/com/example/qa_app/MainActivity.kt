fd1de6a0af78ece41fd5e014cf69299c7b4d13b9
Takahiro Ishikawa <t-ishikawa@i-enter.co.jp>
2019-05-07 10:19:00 +0900

    [update] activity_main.xml. made this activity DrawerLayout
    added DrawerLayout.
    included app_bar_main.xml.
    created nav_header_main.xml(layout)/ activity_main_drawer.xml(menu).

--- app/src/main/java/com/example/qa_app/MainActivity.kt
+++ app/src/main/java/com/example/qa_app/MainActivity.kt
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
 
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity() {
 
