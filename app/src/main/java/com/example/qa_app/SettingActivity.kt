c44b98f022cce779fa14bee744f9255e60b75880
Takahiro Ishikawa <t-ishikawa@i-enter.co.jp>
2019-05-08 10:03:50 +0900

    [add] button onClickListener in SettingActivity.

--- app/src/main/java/com/example/qa_app/SettingActivity.kt
+++ app/src/main/java/com/example/qa_app/SettingActivity.kt
@@ -2,11 +2,22 @@ package com.example.qa_app
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_setting.*
 
 class SettingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
+
+        buttonChange.setOnClickListener {
+            val userName = editTextUserName.text.toString()
+
+            // change userName on the Firebase
+        }
+
+        buttonLogout.setOnClickListener {
+            // call logout.
+        }
     }
 }
