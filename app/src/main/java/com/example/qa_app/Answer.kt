57a37915af38242a5dfc975b53d32681a4d582d0
Takahiro Ishikawa <t-ishikawa@i-enter.co.jp>
2019-05-09 14:25:35 +0900

    [fix] changed name -> userName

--- app/src/main/java/com/example/qa_app/Answer.kt
+++ app/src/main/java/com/example/qa_app/Answer.kt
@@ -2,4 +2,4 @@ package com.example.qa_app
 
 import java.io.Serializable
 
-class Answer(val body: String, val name: String, val uid: String, val answerUid: String) : Serializable
+class Answer(val body: String, val userName: String, val uid: String, val answerUid: String) : Serializable
